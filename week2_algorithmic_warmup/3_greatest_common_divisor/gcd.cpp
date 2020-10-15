b90f2d9e4f48f3dddf6fda25b705e947bb9f8727
Satvik Shrivas <44926681+theSatvik@users.noreply.github.com>
2020-10-15 07:45:37 +0530

    Update gcd.cpp

--- week2_algorithmic_warmup/3_greatest_common_divisor/gcd.cpp
+++ week2_algorithmic_warmup/3_greatest_common_divisor/gcd.cpp
@@ -1,15 +1,12 @@
 #include <iostream>
-
+using namespace std;
 int gcd(int a, int b) {
-  if(b==0)
-    return a;
-  int c = a%b;
-  return gcd(b,c);
+  if(b==0)return a:return gcd(b,a%b);
 }
 
 int main() {
   int a, b;
-  std::cin >> a >> b;
-  std::cout << gcd(a,b) << std::endl;
+  cin >> a >> b;
+  cout << gcd(a,b) << "\n";
   return 0;
 }
